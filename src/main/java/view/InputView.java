90b2830de566674c91976cb939c60223ba357eb4
moa1128 <47878904+moa1128@users.noreply.github.com>
2019-11-10 20:05:01 +0900

    feat:InputView 메소드 static으로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public List<String> inputNames() {
+    public static List<String> inputNames() {
         System.out.println("참여할 사람 이름을 입력하세요");
         String name = scanner.nextLine();
         System.out.println();
         return Arrays.asList(name.split(","));
     }
 
-    public int inputHeight() {
+    public static int inputHeight() {
         System.out.println("최대 사다리의 높이는 몇 개 인가요?");
         int height = scanner.nextInt();
         System.out.println();
