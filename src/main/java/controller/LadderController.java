f05b3c4ff6e9463e770f4bae87b88220d38a8b4f
moa1128 <47878904+moa1128@users.noreply.github.com>
2019-11-10 20:13:20 +0900

    feat:LadderController 변경

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -1,27 +1,26 @@
 package controller;
 
-import model.Lines;
-import model.Name;
-import view.InputView;
-import view.OutputView;
+import model.Ladder;
+import model.Names;
+
+import static view.InputView.inputHeight;
+import static view.InputView.inputNames;
+import static view.OutputView.showResult;
 
 
 public class LadderController {
-    private static final InputView inputView = new InputView();
-    private static final OutputView outView = new OutputView();
-    private Name names = new Name();
 
     public void run() {
         gameStart();
     }
 
     private void gameStart() {
-        do {
-            names.insertNames(inputView.inputNames());
-        } while (names.checkNameLength());
-        int height = inputView.inputHeight();
-        Lines lines = new Lines();
-        lines.addLine(height, names.getNamesSize());
-        outView.showResult(height, names.getMaxNameLength(), names.getNames(), lines);
+        Names names = new Names();
+        names.insertNames(inputNames());
+        names.checkNameLength();
+        int height = inputHeight();
+        Ladder ladder = new Ladder();
+        ladder.addLine(height, names.getNamesSize());
+        showResult(height, names, ladder);
     }
 }
